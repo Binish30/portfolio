d8e54c8bce4f6d5146ff10d860f3a7f9fad7fff4
Binish30 <75393630+Binish30@users.noreply.github.com>
2024-11-04 17:36:45 +0530

    Update RightBanner.js

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -6,7 +6,7 @@ const RightBanner = () => {
     <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
       <img
         className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10"
-        style={{ transform: "translateY(-120px) translateX(50px)" }}
+        style={{ transform: "translateY(-120px) translateX(20px)" }}
         src={bannerImg}
         alt="bannerImg"
       />
