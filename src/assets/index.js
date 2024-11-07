cc38703b8737bd7b44fec538bc9d97bca0b5c1fe
BinishRawal <binish.rawal30@gmail.com>
2024-11-07 13:06:20 +0530

    code

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,4 @@
 import bannerImg from "./images/bannerImg.png";
-import contactImg from "./images/contact/contactImg.png";
 import logo from "./images/logo.png";
 import todolist from "./images/projects/todolist.png";
 import weatherapp from "./images/projects/weatherapp.png";
@@ -8,7 +7,6 @@ import tictactoe from "./images/projects/tictactoe.png";
 
 export {
     bannerImg,
-    contactImg,
     logo,
     todolist,
     weatherapp,
