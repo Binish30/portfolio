27f0bce20c6503765955ab6140aef33ca1814f5b
BinishRawal <binish.rawal30@gmail.com>
2024-11-07 19:08:27 +0530

    Code update

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,7 @@ import todolist from "./images/projects/todolist.png";
 import weatherapp from "./images/projects/weatherapp.png";
 import cricketscoretracker from "./images/projects/cricketscoretracker.png";
 import tictactoe from "./images/projects/tictactoe.png";
+import briyanicenter from "./images/projects/briyanicenter.png";
 
 export {
   bannerImg,
@@ -12,4 +13,5 @@ export {
   weatherapp,
   cricketscoretracker,
   tictactoe,
+  briyanicenter,
 };
